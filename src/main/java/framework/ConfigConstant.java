53621c1a793dcbb75218a902d373dc7947aa479d
Ke Kan <uiojxx@163.com>
2017-07-03 13:43:05 +0800

    add some base files

---
+++ src/main/java/framework/ConfigConstant.java
@@ -0,0 +1,24 @@
+package framework;
+
+/**
+ * 方法作用说明:${enclosing_method}
+ * 使用说明:
+ * 使用注意事项:
+ *
+ * @author kekan
+ *         ${tags}
+ * @since JDK 1.8
+ */
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRIVER = "smart.framework.jdbc.driver";
+    String JDBC_URL = "smart.framework.jdbc.url";
+    String JDBC_USERNAME = "smart.framework.jdbc.username";
+    String JDBC_PASSWORD = "smart.framework.jdbc.password";
+
+    String APP_BASE_PACKAGE = "smart.framework,app.base_package";
+    String APP_JSP_PATH = "smart.framework,app.jsp_path";
+    String APP_ASSET_PATH = "smart.framework,app.asset_path";
+}
