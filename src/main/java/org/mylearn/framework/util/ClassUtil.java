c9aa63d6a90cbd8041220090c655c97c8fbd120f
mark <uiojxx@163.com>
2017-07-08 00:31:36 +0800

    add classLoader & 4 basic annotations

--- src/main/java/org/mylearn/framework/util/ClassUtil.java
+++ src/main/java/org/mylearn/framework/util/ClassUtil.java
@@ -7,10 +7,13 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * 类操作工具类
@@ -60,14 +63,32 @@ public final class ClassUtil {
                     String protocol = url.getProtocol();
                     if (protocol.equals("file")) {
                         String packagePath = url.getPath().replaceAll("%20", "");
-
+                        addClass(classSet, packagePath, packageName);
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if (jarFile != null) {
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if (jarEntryName.endsWith(".class")) {
+                                        String className = jarEntryName.substring(0, jarEntryName.lastIndexOf("."))
+                                                .replaceAll("/", ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         } catch (IOException e) {
-
+            LOGGER.error("get class failure", e);
+            throw new RuntimeException(e);
         }
-        return null;
+        return classSet;
     }
 
     /**
@@ -86,15 +107,19 @@ public final class ClassUtil {
             if (file.isFile()) {
                 String className = fileName.substring(0, fileName.lastIndexOf("."));
                 if (StringUtils.isNotEmpty(packageName)) {
-                    className = packageName + "." +className;
+                    className = packageName + "." + className;
                 }
                 doAddClass(classSet, className);
             } else {
                 String subPackagePath = fileName;
                 if (StringUtils.isNotEmpty(packagePath)) {
-                    subPackagePath = packagePath + "/" +subPackagePath;
+                    subPackagePath = packagePath + "/" + subPackagePath;
                 }
-
+                String subPackageName = fileName;
+                if (StringUtils.isNotEmpty(packageName)) {
+                    subPackageName = packageName + "." + subPackageName;
+                }
+                addClass(classSet, subPackagePath, subPackageName);
             }
         }
     }
